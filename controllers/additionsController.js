ce2f2c86295cb55b739d48227d66f4c1d5477983
public_hysteria <juuzodes@yandex.ru>
2024-11-28 00:58:45 +0300

    additions

--- controllers/additionsController.js
+++ controllers/additionsController.js
@@ -2,17 +2,39 @@ const file = require("../functions/file")
 class Controller {
     async getAll(req,res){
         const nodes = file.readJSON("nodes.json")
-        return res.send(nodes)
+        const obj={}
+        nodes.forEach((element,index) => {
+            obj[index]=element
+        });
+        return res.send(obj)
     }
     async get(req,res){
         const nodes = file.readJSON("nodes.json")
-        return res.send(nodes[req.params.id])
+        const obj={}
+        nodes.forEach((element,index) => {
+            obj[index]=element
+        });
+        return res.send(obj[req.params.id])
     }
     async create(req,res){
-        return res.send(true)
+        const nodes = file.readJSON("nodes.json")
+        nodes.push(req.body)
+        file.update("nodes.json", nodes)
+        const obj={}
+        nodes.forEach((element,index) => {
+            obj[index]=element
+        });
+        return res.send(obj)
     }
     async delete(req,res){
-        return res.send(true)
+        const nodes = file.readJSON("nodes.json")
+        nodes.splice(req.params.id, 1);
+        file.update("nodes.json", nodes)
+        const obj={}
+        nodes.forEach((element,index) => {
+            obj[index]=element
+        });
+        return res.send(obj)
     }
 }
 
